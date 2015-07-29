ec0baa6932c5f04551ae9685e7f2ac098ea46d46
Blueeye1015 <d.ebony.ivory@gmail.com>
2015-07-29 15:42:15 +0800

    v1 完成

--- option.js
+++ option.js
@@ -9,3 +9,18 @@ $('#save').on('click', function () {
 	localStorage.setItem('setting', $('#setting').val())
 	location.reload()
 })
+$('#update').on('click', function () {
+	var regular = JSON.parse(localStorage.getItem('setting'))
+	$.ajax({
+		url: 'http://120.25.151.196/footprint/regular/upload.php',
+		data: {data: regular},
+		type: 'POST',
+		dataType: 'JSON',
+		success: function (data, status) {
+			console.log(data)
+		},
+		error: function (error) {
+			console.log(error)
+		}
+	})
+})
--- popup.js
+++ popup.js
@@ -1,11 +1,45 @@
 'use strict'
 $(function () {
 	if(localStorage.getItem('isLogin') === 'true') {
+		$.get('http://120.25.151.196/footprint/user/myfootprint.php?token=' + localStorage.getItem('token') + '&waiting=true', function (data) {
+			$.each(data.data, function () {
+				var item = '<div><button id="' + this.foot.id + '">删除</button><a href="' + this.foot.url +'">' + this.foot.title + '</a></div>'
+				$('.recent-list').append(item)
+				console.log(this)
+			})
+		})
 		$('#userid').append(localStorage.getItem('userName'))
 		$('.user').remove()
 	} else {
 		$('#userid').append(localStorage.getItem('userId'))
 	}
+
+	$('.recent-list').on('click', function (event) {
+		console.log('http://120.25.151.196/footprint/footprint/remove.php?token=' + localStorage.getItem('token') + '&footprintId=' + event.target.id)
+		$.get('http://120.25.151.196/footprint/footprint/remove.php?token=' + localStorage.getItem('token') + '&footprintId=' + event.target.id, function (data) {
+			console.log(data)
+		})
+	})
+
+	$('#share').on('click', function () {
+		chrome.tabs.getCurrent(function (tab) {
+			var params = {
+				url: localStorage.getItem('currentUrl'),
+				title: localStorage.getItem('currentTitle'),
+				token: localStorage.getItem('token'),
+				initiative: true,
+				time: 0
+			}
+			var url = 'http://120.25.151.196/footprint/footprint/logger.php' + '?' + $.param(params)
+			$.get(url, function (data) {
+				console.log(data)
+				localStorage.removeItem('startTime')
+				localStorage.removeItem('currentUrl')
+				localStorage.removeItem('currentTitle')
+				location.reload()
+			})
+		})
+	})
 
 	$('.register').submit(function () {
 		var userName = $('#userName').val()
@@ -50,7 +84,7 @@ $(function () {
 					localStorage.setItem('isLogin', true)
 					location.href = 'popup.html'
 				} else {
-					$('.error-msg').append(data.error)
+					$('.error-msg').empty().append(data.error)
 				}
 			},
 			error: function (e) {
